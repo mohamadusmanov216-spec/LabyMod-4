98e526818612491762b092c3ae342b400d3c7439
mohamadusmanov216-spec <mohamadusmanov216@gmail.com>
2025-11-17 17:26:10 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,37 @@
 plugins {
     id("java")
-    id("net.labymod.addon") version "0.2.9"
 }
 
 group = "com.axmed555"
 version = "1.0.0"
 
-labymod {
-    addonInfo {
-        namespace = "labyvisuals"
-        displayName = "LabyVisuals"
-        author = "axmed555"
-        description = "Visual effects addon for LabyMod 4"
-        minecraftVersion = "1.20.1"
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
     }
-    productionRelease()
 }
 
 repositories {
     mavenCentral()
+}
+
+// LabyMod API будет доступно при запуске в клиенте
+dependencies {
+    // Минимальные зависимости для компиляции
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+jar {
+    manifest {
+        attributes(
+            "LabyMod-Module" to "com.axmed555.labyvisuals.LabyVisualsAddon",
+            "Version" to version
+        )
+    }
+    
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
